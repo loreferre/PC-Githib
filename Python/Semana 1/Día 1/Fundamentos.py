d9f21901306cc09f1641a9b27d0c697d66aef21d
loreferre <lferreirr@gmail.com>
2023-06-06 13:39:46 -0400

    Tarea Core 1

---
+++ Python/Semana 1/Día 1/Fundamentos.py
@@ -0,0 +1,17 @@
+# Asi se ponene los comentarios
+"""
+Esto 
+es
+un
+comentario
+en
+bloque
+"""
+
+edad = 25
+calificación = 9.6
+
+print (edad, type(edad))
+print( calificación, type(calificación))
+print
+
---
+++ Python/Semana 1/Día 2/Bucles.py
@@ -0,0 +1,5 @@
+for x in 'Hello':
+    print(x)
+
+for x in range(0, 1="constant numeric from-rainbow">0, 2):
+    print(x)
